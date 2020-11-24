ea1b0c23e5d93a1f1f70d5c21d484c06d74390bc
massaro8 <massaro.donato@virgilio.it>
2020-11-24 17:17:52 +0100

    per

--- app/src/main/java/com/example/performance/MainActivity.java
+++ app/src/main/java/com/example/performance/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.performance;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+// Get access to the custom title view
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
