aefe985b6afd79c80321a84fb64aab3dfc472c92
massaro8 <massaro.donato@virgilio.it>
2020-11-25 11:23:55 +0100

    per

--- app/src/main/java/com/example/performance/MainActivity.java
+++ app/src/main/java/com/example/performance/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home,R.id.nav_todolist, R.id.nav_gallery, R.id.nav_note,R.id.nav_media,R.id.nav_news,R.id.nav_calendar)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        //comando per i 3 punti
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
